30e0f2487fecf51f1cfde5806d99c25029cc1a30
Daniil-KOT <d.dulov98@gmail.com>
2022-04-03 20:59:40 +0400

    applied clang-format

--- HW1/udp_client/udp_client.cpp
+++ HW1/udp_client/udp_client.cpp
@@ -3,11 +3,12 @@
 #include <iostream>
 #include <string>
 
+#include <socket_wrapper/socket_class.h>
 #include <socket_wrapper/socket_headers.h>
 #include <socket_wrapper/socket_wrapper.h>
-#include <socket_wrapper/socket_class.h>
 
-int main(int argc, char const *argv[])
+
+int main(int argc, char const* argv[])
 {
     const size_t BUFSIZE = 256;
 
@@ -18,7 +19,7 @@ int main(int argc, char const *argv[])
     }
 
     socket_wrapper::SocketWrapper sock_wrap;
-    const int port = std::stoi(argv[2]);
+    const int                     port = std::stoi(argv[2]);
 
     // creating socket
     socket_wrapper::Socket sock(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
@@ -31,30 +32,29 @@ int main(int argc, char const *argv[])
         return EXIT_FAILURE;
     }
 
-    /* 
+    /*
         two message arrays are needed
-        to correctly process commands 
+        to correctly process commands
         (":exit" command in this case)
-        
+
         it is not useful in this example,
         but if we imagine that the server
-        sends an answer different from the client's message, 
+        sends an answer different from the client's message,
         then the single array will be overwritten
         and the command won't be processed correctly
     */
-    char message_sent[BUFSIZE] = "";
+    char message_sent[BUFSIZE]     = "";
     char message_received[BUFSIZE] = "";
-    bool exit = false;
+    bool exit                      = false;
 
     // setting up server address info
-    struct sockaddr_in server_address = 
-    {
+    struct sockaddr_in server_address = {
         .sin_family = AF_INET,
-        .sin_port = htons(port),
+        .sin_port   = htons(port),
     };
     server_address.sin_addr.S_un.S_addr = inet_addr(argv[1]);
-    socklen_t server_address_len = sizeof(sockaddr_in);
-    ssize_t recv_len = 0;
+    socklen_t server_address_len        = sizeof(sockaddr_in);
+    ssize_t   recv_len                  = 0;
 
     std::cout << "Running UDP client...\n\n";
 
@@ -63,19 +63,25 @@ int main(int argc, char const *argv[])
         std::cout << "$> ";
         std::cin.getline(message_sent, BUFSIZE);
 
-        sendto(sock, message_sent, strlen(message_sent), 0, 
-                reinterpret_cast<const sockaddr*>(&server_address),
-                server_address_len);
-        
-        recv_len = recvfrom(sock, message_received, sizeof(message_received) - 1, 0,
+        sendto(sock,
+               message_sent,
+               strlen(message_sent),
+               0,
+               reinterpret_cast<const sockaddr*>(&server_address),
+               server_address_len);
+
+        recv_len = recvfrom(sock,
+                            message_received,
+                            sizeof(message_received) - 1,
+                            0,
                             reinterpret_cast<sockaddr*>(&server_address),
                             &server_address_len);
-                            
+
         if (recv_len > 0)
         {
             std::cout << message_received << std::endl;
         }
-        
+
         if (message_sent[0] == ':')
         {
             std::string command = message_sent;
