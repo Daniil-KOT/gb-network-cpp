32970368dc7fbd72d7a9a325865e05c930db1600
Daniil-KOT <d.dulov98@gmail.com>
2022-04-21 16:46:30 +0400

    udated client

--- HW5/http-client/mainwindow.h
+++ HW5/http-client/mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include "query-header.h"
 
 class QTcpSocket;
 
@@ -20,11 +21,28 @@ public:
     MainWindow(QWidget* parent = nullptr);
     ~MainWindow();
 
+private:
+
+    void create_get_header();
+
+    void create_post_header();
+
+    void send_query();
+
+    void receive_response();
+
 private slots:
-    void on_pushButton_clicked();
+    void on_connect_clicked();
+
+    void on_GET_clicked();
+
+    void on_POST_clicked();
 
 private:
     Ui::MainWindow* ui;
     QTcpSocket*     socket_;
+    QString query_;
+    QString content_;
+    QueryHeader header_;
 };
 #endif // MAINWINDOW_H
